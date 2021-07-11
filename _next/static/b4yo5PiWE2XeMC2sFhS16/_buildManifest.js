a8b4dc987a0057ae7f8ccd5b436db397edc34e9a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-11 15:26:30 +0000

    Deploy rahuldkjain/animations to github.com/rahuldkjain/animations.git:gh-pages

---
+++ _next/static/b4yo5PiWE2XeMC2sFhS16/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-ed5688a48aba3b621fe0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c452af22deaafabab055.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
